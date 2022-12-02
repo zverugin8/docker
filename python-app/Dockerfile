e97bf08d91089d27510c22dabbb6c8ee80877e90
Sergey <zverugin8@gmail.com>
2022-12-02 22:40:46 +0300

    dockerfile commit

---
+++ python-app/Dockerfile
@@ -0,0 +1,4 @@
+FROM python:alpine
+WORKDIR /app
+COPY . .
+CMD [ "python", "main.py" ]
---
+++ python-app/main.py
@@ -0,0 +1,6 @@
+import calendar
+print('Welcome to calendar\n')
+year = int(input('Enter the year: '))
+month = int(input('Enter the month: '))
+print(calendar.month(year,month))
+print('Bye!')
